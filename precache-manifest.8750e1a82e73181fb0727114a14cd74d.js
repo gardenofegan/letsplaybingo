461a9c8a6d7165809a351c230bc5b4e55cf3491b
jegan <justin.egan@online-rewards.com>
2019-03-30 07:42:04 -0400

    Updates

---
+++ precache-manifest.8750e1a82e73181fb0727114a14cd74d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a6f7124fc364ee38beca9cf869cec60",
+    "url": "/letsplaybingo/static/media/logo.5a6f7124.svg"
+  },
+  {
+    "revision": "36ea046c21b9d3dfdb99",
+    "url": "/letsplaybingo/static/js/runtime~main.6b332143.js"
+  },
+  {
+    "revision": "bd52465266f65d62b10b",
+    "url": "/letsplaybingo/static/js/main.c246d8d6.chunk.js"
+  },
+  {
+    "revision": "aa4b0c43502bdc6e1297",
+    "url": "/letsplaybingo/static/js/2.61dc7f9f.chunk.js"
+  },
+  {
+    "revision": "bd52465266f65d62b10b",
+    "url": "/letsplaybingo/static/css/main.7bba9988.chunk.css"
+  },
+  {
+    "revision": "aa4b0c43502bdc6e1297",
+    "url": "/letsplaybingo/static/css/2.1090ef56.chunk.css"
+  },
+  {
+    "revision": "8deea17de2b605c640dfb051242ae796",
+    "url": "/letsplaybingo/index.html"
+  }
+];
